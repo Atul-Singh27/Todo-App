b028813feaf39015e47db5f98605fa68668747b9
Atul-Singh27 <64123403+Atul-Singh27@users.noreply.github.com>
2026-01-29 08:42:44 +0530

    Add files via upload

---
+++ todos.py
@@ -0,0 +1,7 @@
+todos = [
+    {"id": 1, "title": "GYM", "complete": True},
+    {"id": 2, "title": "Meet", "complete": False},
+    {"id": 3, "title": "Study", "complete": False},
+]
+
+next_id = 4
